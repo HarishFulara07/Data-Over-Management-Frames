383dd4627723f64ccfb2c6bd89348cf4fb8e7a03
Gursimran Singh <gursimransingh.gsh@gmail.com>
2018-06-19 15:36:10 +0530

    make-toolchains script written.

--- android/xc-scripts/make-ndk-toolchains/make-ndk-toolchains.py
+++ android/xc-scripts/make-ndk-toolchains/make-ndk-toolchains.py
@@ -1,9 +1,11 @@
 import os
+import subprocess
 
 __desc__ = \
 	"""
-	A handy script to make NDK standalone toolchains for a given api level and stl
-		- for all abi
+	A handy script to make NDK standalone toolchains.
+	Three arguments possible: arch, api and stl
+	Any unspecified argument is iterated over all possible values
 	"""
 
 import argparse
@@ -15,13 +17,32 @@ _ARCH = [
 	"x86_64"
 ]
 
+_API = [
+	14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
+]
+
+_STL = [
+	"gnustl",
+	"libc++",
+	"stlport"
+]
+
 
 def argument_parser():
 	_parser = argparse.ArgumentParser(__desc__)
 	_parser.add_argument(
+		'--ndk-home',
+		dest = 'ndk_home',
+		action = 'store',
+		type = str,
+		required = True,
+		help = "the path to ndk installation dir"
+	)
+	_parser.add_argument(
 		'--dir',
 		dest = 'output_dir',
 		action = 'store',
+		type = str,
 		required = True,
 		help = "the output directory"
 	)
@@ -30,27 +51,84 @@ def argument_parser():
 		dest = 'api',
 		action = 'store',
 		type = int,
-		required = True,
-		help = "the api level"
+		help = "the api level to build for"
+	)
+	_parser.add_argument(
+		'--stl',
+		dest = 'stl',
+		action = 'store',
+		type = str,
+		help = "the standard template library to use"
+	)
+	_parser.add_argument(
+		'--arch',
+		dest = 'arch',
+		action = 'store',
+		type = str,
+		help = "the architecture to build for"
 	)
 	return _parser
 
 
-def make(args):
-	output_dir = os.path.abspath(args.output_dir)
-	if os.path.exists(output_dir):
-		print("Output directory already exists!")
-		exit(1)
-	else:
-		os.mkdir(output_dir)
-		pass
+def make(ndk_home, output_dir, _api_list, _stl_list, _arch_list):
+	for _api in _api_list:
+		_api_dir = os.path.join(output_dir, "android-{:d}".format(_api))
+		os.mkdir(_api_dir)
 
-	# make
+		for _stl in _stl_list:
+			_stl_dir = os.path.join(_api_dir, "{:s}".format(_stl))
+			os.mkdir(_stl_dir)
+
+			for _arch in _arch_list:
+				_arch_dir = os.path.join(_stl_dir, "{:s}".format(_arch))
+				os.mkdir(_arch_dir)
+
+				command = "python {:s}/build/tools/make-standalone-toolchains.py --api {:d} --stl {:s} --arch {:s} " \
+				          "--install-dir {:s}".format(str(ndk_home), _api, _stl, _arch, str(_arch_dir))
+
+				print("Running `{:s}`".format(command))
+				rc = subprocess.call(
+					args = command,
+					stdin = subprocess.PIPE,
+					stderr = subprocess.PIPE,
+					shell = True
+				)
+				print("Return-Code: {:d}".format(rc))
+				print()
+
+				if rc != 0:
+					os.rmdir(_arch_dir)
+
 	pass
 
 
 if __name__ == '__main__':
 	parser = argument_parser()
 	arguments = parser.parse_args()
-	make(arguments)
+
+	# Sanitize
+	# ndk-home
+	arguments.ndk_home = os.path.abspath(arguments.ndk_home)
+	# output-dir
+	arguments.output_dir = os.path.abspath(arguments.output_dir)
+	# api
+	api_list = list()
+	if arguments.api is not None:
+		api_list.append(arguments.api)
+	else:
+		api_list.extend(_API)
+	# stl
+	stl_list = list()
+	if arguments.stl is not None:
+		stl_list.append(arguments.stl)
+	else:
+		stl_list.extend(_STL)
+	# stl
+	arch_list = list()
+	if arguments.arch is not None:
+		arch_list.append(arguments.arch)
+	else:
+		arch_list.extend(_ARCH)
+
+	make(arguments.ndk_home, arguments.output_dir, api_list, stl_list, arch_list)
 	pass
