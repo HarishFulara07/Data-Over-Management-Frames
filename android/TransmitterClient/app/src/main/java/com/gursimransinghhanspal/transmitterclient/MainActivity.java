854d0a6a3ef994b68f6688496619cae285ae4bdc
Gursimran Singh <gursimransingh.gsh@gmail.com>
2018-06-26 21:05:00 +0530

    Updated the app layout.

--- android/TransmitterClient/app/src/main/java/com/gursimransinghhanspal/transmitterclient/MainActivity.java
+++ android/TransmitterClient/app/src/main/java/com/gursimransinghhanspal/transmitterclient/MainActivity.java
@@ -3,16 +3,15 @@ package com.gursimransinghhanspal.transmitterclient;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.net.wifi.WifiManager;
-import android.os.Build;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,326 +23,269 @@ import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("jni-handler");
-    }
+	private static final String TAG = MainActivity.class.getSimpleName();
+	private static final int ONE_TIME_BROADCAST_DELIVERY = 1;
+	private static final int GUARANTEED_DELIVERY_EFFORT = 2;
 
-    private EditText mIeCountEditText;
-    private EditText mIeDataEditText;
-    private TextView mOutputLog;
+	SwitchCompat mGuaranteedDeliverySwitch;
+	SwitchCompat mOneTimeBroadcastSwitch;
+	private EditText mIeDataEditText;
+	private TextView mOutputLog;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	/* ***
+	 * Load the native libraries on application startup
+	 */
+	static {
+		System.loadLibrary("jni-handler");
+	}
 
-        ImageButton mIeCountIncreaseButton = findViewById(R.id.resLayout_activityMain_ieCountSpinner_incButton);
-        ImageButton mIeCountDecreaseButton = findViewById(R.id.resLayout_activityMain_ieCountSpinner_decButton);
-        mIeCountEditText = findViewById(R.id.resLayout_activityMain_ieCountInput);
-        mIeDataEditText = findViewById(R.id.resLayout_activityMain_ieDataInput);
-        mOutputLog = findViewById(R.id.resLayout_activityMain_outputLog);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        mIeCountIncreaseButton.setOnClickListener(this);
-        mIeCountDecreaseButton.setOnClickListener(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main_options, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.resMenu_activityMain_transmit:
-                onTransmit();
-                return true;
-        }
-        return false;
-    }
+		mGuaranteedDeliverySwitch = findViewById(R.id.resLayout_activityMain_guaranteedDelivery_switch);
+		mOneTimeBroadcastSwitch = findViewById(R.id.resLayout_activityMain_oneTimeBroadcast_switch);
+		mIeDataEditText = findViewById(R.id.resLayout_activityMain_ieDataInput);
+		mOutputLog = findViewById(R.id.resLayout_activityMain_outputLog);
 
 
-    @Override
-    public void onClick(View view) {
+		// default values
+		mGuaranteedDeliverySwitch.setChecked(true);
+		mOneTimeBroadcastSwitch.setChecked(false);
 
-        switch (view.getId()) {
-            case R.id.resLayout_activityMain_ieCountSpinner_incButton:
-                onIncreaseCount();
-                break;
+		// click listeners
+		mGuaranteedDeliverySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+				mOneTimeBroadcastSwitch.setChecked(!b);
+			}
+		});
+		mOneTimeBroadcastSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+				mGuaranteedDeliverySwitch.setChecked(!b);
+			}
+		});
+	}
 
-            case R.id.resLayout_activityMain_ieCountSpinner_decButton:
-                onDecreaseCount();
-                break;
-        }
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main_options, menu);
+		return true;
+	}
 
-    /* *
-     * onClick Handlers.
-     */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.resMenu_activityMain_transmit:
+				onTransmit();
+				return true;
+		}
+		return false;
+	}
 
-    private void onIncreaseCount() {
-        String countAsString = mIeCountEditText.getText().toString().trim();
-        Integer count;
-        try {
-            count = Integer.parseInt(countAsString);
-        } catch (NumberFormatException nfe) {
-            Toast.makeText(
-                    MainActivity.this,
-                    "Invalid IE count!",
-                    Toast.LENGTH_SHORT
-            ).show();
-            return;
-        }
 
-        if (count < 1) {
-            count = 1;
-        } else {
-            count += 1;
-        }
+	/**
+	 * Called when transmit button is clicked
+	 * - Parses the Input data
+	 * - Enabled WIFI if turned off
+	 * - Opens a dialog selector to
+	 */
+	private void onTransmit() {
 
-        if (count > 2) {
-            count = 2;
-        }
+		clearLogs();
 
-        mIeCountEditText.setText(String.format(Locale.US, "%d", count));
-    }
+		/*
+		 * Parse the input
+		 */
+		final UserInput userInput = parseInput();
+		if (userInput == null) {
+			return;
+		}
 
-    private void onDecreaseCount() {
-        String countAsString = mIeCountEditText.getText().toString().trim();
-        Integer count;
-        try {
-            count = Integer.parseInt(countAsString);
-        } catch (NumberFormatException nfe) {
-            Toast.makeText(
-                    MainActivity.this,
-                    "Invalid IE count!",
-                    Toast.LENGTH_SHORT
-            ).show();
-            return;
-        }
+		/* *
+		 * Enable WiFi
+		 */
+		WifiManager manager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+		if (manager == null) {
+			appendToLogs(LogTag.ERR, "Could not acquire Context.WIFI_SERVICE!");
+			return;
+		}
 
-        if (count <= 1) {
-            count = 1;
-        } else {
-            count -= 1;
-        }
+		if (!manager.isWifiEnabled()) {
+			appendToLogs(LogTag.INF, "Device WIFI is not enabled. Enabling...");
+			manager.setWifiEnabled(true);
+		} else {
+			appendToLogs(LogTag.INF, "Device WIFI is enabled.");
+		}
+		appendToLogs(LogTag.EMPTY, null);
 
-        mIeCountEditText.setText(String.format(Locale.US, "%d", count));
-    }
+		/* *
+		 * Find wireless interface name
+		 */
+		appendToLogs(LogTag.INF, "Detecting all available interfaces...");
+		final ArrayList<NetworkInterface> networkInterfaces = new ArrayList<>();
+		try {
+			for (Enumeration<NetworkInterface> list = NetworkInterface.getNetworkInterfaces(); list.hasMoreElements(); ) {
+				networkInterfaces.add(list.nextElement());
+			}
+		} catch (SocketException | NullPointerException ignored) {
 
-    /**
-     * Called when transmit button is clicked
-     * - Parses the Input data
-     * - Enabled WIFI if turned off
-     * - Opens a dialog selector to
-     */
-    private void onTransmit() {
+		}
 
-        clearLogs();
+		// corner cases!
+		if (networkInterfaces.size() == 0) {
+			appendToLogs(LogTag.ERR, "No interfaces found!");
+			return;
+		}
+		if (networkInterfaces.size() == 1) {
+			appendToLogs(LogTag.INF, "Only single interface available. Proceeding...");
+			onInterfaceSelected(networkInterfaces.get(0), userInput.data, userInput.effort);
+			return;
+		}
 
-        /*
-         * Parse the input
-         */
-        final InputData ieData = parseInput();
-        if (ieData == null) {
-            return;
-        }
+		// sort for visual ease
+		Collections.sort(networkInterfaces, new Comparator<NetworkInterface>() {
+			@Override
+			public int compare(NetworkInterface networkInterface, NetworkInterface t1) {
+				return networkInterface.getIndex() - t1.getIndex();
+			}
+		});
 
-        /* *
-         * Enable WiFi
-         */
-        WifiManager manager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        if (manager == null) {
-            appendToLogs(LogTag.ERR, "Could not acquire Context.WIFI_SERVICE!");
-            return;
-        }
+		// show list to choose
+		String[] interfaceNames = new String[networkInterfaces.size()];
+		for (int i = 0; i < networkInterfaces.size(); i++) {
+			interfaceNames[i] = String.format(Locale.US, "%d. %s", networkInterfaces.get(i).getIndex(), networkInterfaces.get(i).getDisplayName());
+		}
 
-        if (!manager.isWifiEnabled()) {
-            appendToLogs(LogTag.INF, "Device WIFI is not enabled. Enabling...");
-            manager.setWifiEnabled(true);
-        } else {
-            appendToLogs(LogTag.INF, "Device WIFI is enabled.");
-        }
-        appendToLogs(LogTag.EMPTY, null);
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Select Network Interface");
+		builder.setItems(
+				interfaceNames,
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialogInterface, int i) {
+						onInterfaceSelected(networkInterfaces.get(i), userInput.data, userInput.effort);
+					}
+				}
+		);
+		builder.create().show();
+	}
 
-        /* *
-         * Find wireless interface name
-         */
-        appendToLogs(LogTag.INF, "Detecting all available interfaces...");
-        final ArrayList<NetworkInterface> networkInterfaces = new ArrayList<>();
-        try {
-            for (Enumeration<NetworkInterface> list = NetworkInterface.getNetworkInterfaces(); list.hasMoreElements(); ) {
-                networkInterfaces.add(list.nextElement());
-            }
-        } catch (SocketException | NullPointerException ignored) {
+	/* *
+	 * Helper Methods
+	 */
 
-        }
+	private UserInput parseInput() {
 
-        // corner cases!
-        if (networkInterfaces.size() == 0) {
-            appendToLogs(LogTag.ERR, "No interfaces found!");
-            return;
-        }
-        if (networkInterfaces.size() == 1) {
-            appendToLogs(LogTag.INF, "Only single interface available. Proceeding...");
-            onInterfaceSelected(networkInterfaces.get(0), ieData.data, ieData.effort);
-            return;
-        }
+		int inputEffort;
+		if (mGuaranteedDeliverySwitch.isChecked()) {
+			inputEffort = GUARANTEED_DELIVERY_EFFORT;
+		} else if (mOneTimeBroadcastSwitch.isChecked()) {
+			inputEffort = ONE_TIME_BROADCAST_DELIVERY;
+		} else {
+			Toast.makeText(
+					MainActivity.this,
+					"Invalid effort value!",
+					Toast.LENGTH_SHORT
+			).show();
+			return null;
+		}
 
-        // sort for visual ease
-        Collections.sort(networkInterfaces, new Comparator<NetworkInterface>() {
-            @Override
-            public int compare(NetworkInterface networkInterface, NetworkInterface t1) {
-                return networkInterface.getIndex() - t1.getIndex();
-            }
-        });
+		String inputData = mIeDataEditText.getText().toString().trim();
+		if (TextUtils.isEmpty(inputData)) {
+			Toast.makeText(
+					MainActivity.this,
+					"Data can't be empty!",
+					Toast.LENGTH_SHORT
+			).show();
+			return null;
+		}
 
-        // show list to choose
-        String[] interfaceNames = new String[networkInterfaces.size()];
-        for (int i = 0; i < networkInterfaces.size(); i++) {
-            interfaceNames[i] = String.format(Locale.US, "%d. %s", networkInterfaces.get(i).getIndex(), networkInterfaces.get(i).getDisplayName());
-        }
+		UserInput userInput = new UserInput();
+		userInput.effort = inputEffort;
+		userInput.data = inputData;
+		return userInput;
+	}
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Select Network Interface");
-        builder.setItems(
-                interfaceNames,
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        onInterfaceSelected(networkInterfaces.get(i), ieData.data, ieData.effort);
-                    }
-                }
-        );
-        builder.create().show();
-    }
+	/**
+	 * Called when the user selects an Interface from the list
+	 * <p>
+	 * - Calls the native code to stuff Information Elements
+	 * - Initiates a WIFI scan after the stuffing
+	 */
+	private void onInterfaceSelected(NetworkInterface networkInterface, String ieData, int effort) {
 
-    /* *
-     * Helper Methods
-     */
+		appendToLogs(LogTag.INF, String.format("<%s> interface selected.", networkInterface.getDisplayName()));
+		appendToLogs(LogTag.INF, "Attempting to stuff Information Elements...");
 
-    private InputData parseInput() {
-        String countAsString = mIeCountEditText.getText().toString().trim();
-        Integer count;
-        try {
-            count = Integer.parseInt(countAsString);
-        } catch (NumberFormatException nfe) {
-            Toast.makeText(
-                    MainActivity.this,
-                    "Invalid effort!",
-                    Toast.LENGTH_SHORT
-            ).show();
-            return null;
-        }
+		// call native probe stuffing code here!
+		setInformationElementsInProbeRequests(
+				networkInterface.getName(),
+				ieData,
+				effort
+		);
+		appendToLogs(LogTag.INF, "Stuffing done!");
 
-        if (count != 1 && count != 2) {
-            Toast.makeText(
-                    MainActivity.this,
-                    "Effort can only be 1 or 2!",
-                    Toast.LENGTH_SHORT
-            ).show();
-            return null;
-        }
+		// initiate scan
+		WifiManager manager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+		if (manager == null) {
+			appendToLogs(LogTag.ERR, "Could not acquire Context.WIFI_SERVICE!");
+			return;
+		}
+		appendToLogs(LogTag.INF, "Initiating WIFI scan.");
+		manager.startScan();
+	}
 
-        String dataAsSingleString = mIeDataEditText.getText().toString().trim();
-        if (TextUtils.isEmpty(dataAsSingleString)) {
-            Toast.makeText(
-                    MainActivity.this,
-                    "Data can't be empty!",
-                    Toast.LENGTH_SHORT
-            ).show();
-            return null;
-        }
+	private void clearLogs() {
+		mOutputLog.setText(null);
+	}
 
-        InputData inputData = new InputData();
-        inputData.effort = count;
-        inputData.data = dataAsSingleString;
-        return inputData;
-    }
+	private void appendToLogs(LogTag tag, String string) {
+		String logs = mOutputLog.getText().toString();
 
-    /**
-     * Called when the user selects an Interface from the list
-     * <p>
-     * - Calls the native code to stuff Information Elements
-     * - Initiates a WIFI scan after the stuffing
-     */
-    private void onInterfaceSelected(NetworkInterface networkInterface, String ieData, int effort) {
+		switch (tag) {
+			case ERR:
+				logs += "ERR: " + string;
+				break;
 
-        appendToLogs(LogTag.INF, String.format("<%s> interface selected.", networkInterface.getDisplayName()));
-        appendToLogs(LogTag.INF, "Attempting to stuff Information Elements...");
+			case INF:
+				logs += "INF: " + string;
+				break;
 
-        // call native probe stuffing code here!
-        setInformationElementsInProbeRequests(
-                networkInterface.getName(),
-                ieData,
-                effort
-        );
-        appendToLogs(LogTag.INF, "Stuffing done!");
+			case EMPTY:
+				break;
+		}
 
-        // initiate scan
-        WifiManager manager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        if (manager == null) {
-            appendToLogs(LogTag.ERR, "Could not acquire Context.WIFI_SERVICE!");
-            return;
-        }
-        appendToLogs(LogTag.INF, "Initiating WIFI scan.");
-        manager.startScan();
-    }
+		logs += "\n";
+		mOutputLog.setText(logs);
+	}
 
-    private void clearLogs() {
-        mOutputLog.setText(null);
-    }
+	private class UserInput {
+		int effort;
+		String data;
+	}
 
-    private void appendToLogs(LogTag tag, String string) {
-        String logs = mOutputLog.getText().toString();
+	/* *
+	 * Private Helper Enum
+	 */
+	private enum LogTag {
+		ERR,
+		INF,
+		EMPTY
+	}
 
-        switch (tag) {
-            case ERR:
-                logs += "ERR: " + string;
-                break;
+	/**
+	 * Native Methods
+	 */
 
-            case INF:
-                logs += "INF: " + string;
-                break;
+	// default method
+	// good to check that ndk integration is actually working!
+	public native String stringFromJNI();
 
-            case EMPTY:
-                break;
-        }
-
-        logs += "\n";
-        mOutputLog.setText(logs);
-    }
-
-    private class InputData {
-        int effort;
-        String data;
-    }
-
-    /* *
-     * Private Helper Enum
-     */
-    private enum LogTag {
-        ERR,
-        INF,
-        EMPTY
-    }
-
-    /* *
-     * Native Methods
-     */
-    static {
-        System.loadLibrary("jni-handler");
-    }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    native void setInformationElementsInProbeRequests(String interfaceName, String ieData, int effort);
+	// our main method
+	native void setInformationElementsInProbeRequests(String interfaceName, String ieData, int effort);
 }
